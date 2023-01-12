e82fd4167e3a1ec17641c6b18093709c8f2cd4ee
Saurabhvishv <saurabh2vishv@gmail.com>
2023-01-12 15:15:52 +0530

    editReset

--- src/nftBuySell/metaMask.jsx
+++ src/nftBuySell/metaMask.jsx
@@ -971,6 +971,7 @@ const MetaMask = () => {
     }
     const reset = () => {
         setAccount([]);
+        setSessions([]);
     };
     const faucetAddress = '0x88250f772101179a4ecfaa4b92a983676a3ce445'
     const sendTransaction = () => provider.request({
