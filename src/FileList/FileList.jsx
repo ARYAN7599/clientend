fb0b3186e354040124804033758998b337277d8b
Saurabhvishv <saurabh2vishv@gmail.com>
2023-01-17 13:03:08 +0530

    https

--- src/FileList/FileList.jsx
+++ src/FileList/FileList.jsx
@@ -3,17 +3,17 @@ import React, { useState } from "react";
 import FileItem from './../FileItem/FileItem'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Col, Row, Image } from 'react-bootstrap'
-const baseURL= "http://blockchaintimes.live/api/images/";
+const baseURL= "https://blockchaintimes.live/api/images/";
 
 const FileList = ({ files, getFile }) => {
     const getFileHandler = (_name) => {
-        axios.get(`http://blockchaintimes.live/api/images/${_name}`)
+        axios.get(`https://blockchaintimes.live/api/images/${_name}`)
             .then((res) => getFile("http://blockchaintimes.live/api/images/"+_name))
             .catch((err) => console.error(err));
     }
     const [imageArray, setPost] = useState(null);
     const get = async () => {
-        axios.get("http://blockchaintimes.live/api").then((res) => {
+        axios.get("https://blockchaintimes.live/api").then((res) => {
             setPost(res.data);
         });
     }
